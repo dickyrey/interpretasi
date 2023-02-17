493e32aecb54a8ae7d5154d596463de75783911f
dickyrey <dick.rey97@gmail.com>
2023-02-17 14:28:53 +0700

    fixed token

--- lib/src/data/datasources/auth_data_source.dart
+++ lib/src/data/datasources/auth_data_source.dart
@@ -37,7 +37,7 @@ class AuthDataSourceImpl extends AuthDataSource {
     final isSignedIn = await googleSignIn.isSignedIn();
     final prefs = await SharedPreferences.getInstance();
     final token = prefs.getString(Const.token);
-    
+
     final header = {
       'Authorization': 'Bearer $token',
       'Accept': 'application/json',
@@ -63,6 +63,7 @@ class AuthDataSourceImpl extends AuthDataSource {
         return false;
       }
     } else {
+      await prefs.remove(Const.token);
       throw ServerException(ExceptionMessage.internetNotConnected);
     }
   }
